581e8214af00bc51dca83725493a564c9df0be4d
timmyjoe182-maker <timmyjoe182@gmail.com>
2026-05-18 16:26:42 -0700

    Update supabaseClient.js

--- brandthat-ai-full-project/src/supabaseClient.js
+++ brandthat-ai-full-project/src/supabaseClient.js
@@ -1,6 +1,6 @@
-import { createClient } from "@supabase/supabase-js";
+iimport { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "https://vfnkmabnocbwawbvdxfo.supabase.co",
+  "https://vfnkmabnocbwawbdvxfo.supabase.co",
   "sb_publishable_Hc3jSEKgrOf1ntpRxnVJzg_Ttr1oAuk"
 );
