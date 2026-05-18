352ee9406b95e895fdceabdcf5d81fda176c7245
timmyjoe182-maker <timmyjoe182@gmail.com>
2026-05-18 13:13:42 -0700

    Delete brandthat-ai-full-project/src/brandthat-ai-full-project/src directory

--- brandthat-ai-full-project/src/brandthat-ai-full-project/src/supabaseClient.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://vfnkmabnocbwawbdvxfo.supabase.co";
-const supabaseAnonKey = "sb_publishable_Hc3jSEKgrOf1ntpRxnVJzg_Ttr1oAuk";
-
-export const supabase = createClient(
-  supabaseUrl,
-  supabaseAnonKey
-);
